9302b59c722a6feca625256260a0868875ab3076
Ilias Bartolini <ilias.bartolini@gmail.com>
2018-11-02 19:24:55 +0100

    Circle CI: add rsync

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,5 +3,8 @@
 echo "Add remote ssh key fingerprints"
 ssh-keyscan privacy-seed.org >> ~/.ssh/known_hosts
 
+echo "Install rsync"
+sudo apt-get install rsync --yes
+
 echo "Sync new files"
 rsync -avi --delete-after -e "ssh" _site/* $PS_USER@privacy-seed.org:$PS_DEPLOY_PATH
