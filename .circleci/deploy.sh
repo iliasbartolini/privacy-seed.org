bb1a86bf2ea41c134cd60aea6648054d08650919
Ilias Bartolini <ilias.bartolini@gmail.com>
2018-11-02 19:51:52 +0100

    Remove unused files

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -7,4 +7,4 @@ echo "Install rsync"
 sudo apt-get install rsync --yes
 
 echo "Sync new files"
-rsync -avi --delete-after -e "ssh" _site/* $PS_USER@privacy-seed.org:$PS_DEPLOY_PATH
+rsync -avi --delete -e "ssh" _site/* $PS_USER@privacy-seed.org:$PS_DEPLOY_PATH
