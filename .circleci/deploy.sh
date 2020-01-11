c876d23e7dfa063e09713a94d09fd98c669fbbb5
Ilias Bartolini <ilias.bartolini@riseup.net>
2020-01-11 21:43:49 +0100

    Fix deploy script: install pip3

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-echo "Add remote ssh key fingerprints"
-sudo pip install awscli
+echo "Install AWS cli"
+sudo apt-get install pip3 --yes                                                           
+pip3 install awscli --upgrade --user                                                           
 
 echo "Sync new files"
 aws s3 sync _site s3://privacy-seed.org/ --delete
